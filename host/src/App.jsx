972fbb40853ccac03f4f81e5ff7b73fc271a99fb
KshitijV97 <writetokshitij@gmail.com>
2023-07-26 08:34:26 +0530

    Fixed imports

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+import Button from "remoteApp/Button"; // `remoteApp` is substituted by Configuration
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = React.useState(0);
 
 	return (
 		<>
 			<h1>Host application</h1>
 			<div className='card'>
+				<Button />
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
